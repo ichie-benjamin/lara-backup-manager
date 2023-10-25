8119534f5af4205a6baa057539e7f1d7c52d41ca
benny <benjaminchukwudi0@gmail.com>
2023-10-25 21:05:19 +0100

    init commit

--- src/Migrations/2023_10_24_000000_create_lara_backup_verify_table.php
+++ src/Migrations/2023_10_24_000000_create_lara_backup_verify_table.php
@@ -14,7 +14,7 @@ class CreateLaraBackupVerifyTable extends Migration
     public function up()
     {
         Schema::create('verifybackup', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             $table->string('verify_status');
         });
     }
